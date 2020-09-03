dfee986a8b5de91a27a7e71956bc711822e7eb27
Luis Geniole <luis.geniole@gmail.com>
2020-09-03 00:40:40 -0300

    First attempt, trying it out.

---
+++ omni.js
@@ -0,0 +1,25 @@
+const omni = [
+  "7=191622",
+  "8=E1E1E6",
+  "9=67E480",
+  "a=p",
+  "aa=78D1E1",
+  "ae=t",
+  "af=1",
+  "ai=1",
+  "f=1",
+  "j=191622",
+  "m=l",
+  "o=s",
+  "s=m",
+  "t=p",
+  "u=-1",
+  "w=n",
+  "x=E7DE79",
+  "y=41414D",
+];
+
+for (var i = 0; i < omni.length; i++) document.cookie = omni[i];
+
+alert("Omni theme has been successfully install for DuckDuckGo!");
+location.reload();
